b3920bc205a5abb63279824fd7a397b913868f37
fed1k <allamurotovcoder@mail.ru>
2025-03-17 01:39:59 +0500

    check exact log

--- controllers/resultController.js
+++ controllers/resultController.js
@@ -82,7 +82,7 @@ exports.createResult = async (req, res) => {
     const result = await Result.create();
     res.status(200).json({ status: 200, id: result.id });
   } catch (err) {
-    res.status(500).json({ error: 'Error creating result' });
+    res.status(500).json({ error: err.message });
   }
 }
 
