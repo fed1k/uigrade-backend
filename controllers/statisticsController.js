8d199990e845e3e6ce5d9bd2419ebf2904c74e26
fed1k <allamurotovcoder@mail.ru>
2025-03-21 03:17:29 +0500

    statistics

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -1,14 +1,34 @@
 const Statistics = require('../models/statistic');
+const Result = require('../models/result');
+const { Sequelize } = require('sequelize');
 
 exports.getStatistics = async (req, res) => {
     try {
+        const [totalResults, completeResults, averageResult] = await Promise.all([
+            Result.count(),
+            Result.count({ where: { status: 'complete' } }),
+            Result.findOne({
+                attributes: [
+                    [Sequelize.fn('AVG', Sequelize.col('score')), 'average_score']
+                ]
+            })
+        ]);
+
+        // Extract the average score value
+        const averageScore = averageResult ? averageResult.get('average_score') : 0;
+
         let stats = await Statistics.findAll();
-        console.log(stats)
-        res.status(200).json(stats)
+        res.status(200).json({ 
+            status: 200, 
+            tg_visit_count: stats[0].tg_visit_count,
+            totalResults, 
+            completeResults, 
+            averageScore 
+        });
     } catch (err) {
-        res.status(500).json({ error: 'Error fetching statistics level' });
+        res.status(500).json({ error: err.message });
     }
-}
+};
 
 exports.addTgVisitorCount = async (req, res) => {
     try {
